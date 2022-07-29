c35bb52a4bcf25a8cfbb352a8882a030e66e498e
E K <emmanuelkoupoh@gmail.com>
2022-07-29 15:37:00 +0000

    @TODO:

--- assignments/palindrome/main.py
+++ assignments/palindrome/main.py
@@ -2,6 +2,9 @@ from utils import is_palindrome
 
 
 def main():
+    #
+    # @TODO: Implement it
+    #
     pass
 
 
--- assignments/palindrome/utils.py
+++ assignments/palindrome/utils.py
@@ -1,4 +1,6 @@
 def is_palindrome(sequence):
     """function returning if a sequence is a palindrome"""
-
+    #
+    # @TODO: Implement it
+    #
     return
